272bed69f27f95a8f7877dd4e3a1aaef7b30eefe
kelseyjdickson <kelseymedved@gmail.com>
2020-08-24 16:30:46 -0400

    Done.

--- src/Bomb.js
+++ src/Bomb.js
@@ -1,1 +1,28 @@
 // your Bomb code here!
+import React from 'react'
+
+
+class Bomb extends React.Component {
+    constructor(props) {
+        super()
+        this.state = {
+            secondsLeft: props.initialCount
+        }
+       
+    }
+    
+
+    render () {
+
+        if (this.props.initialCount === 0) {
+            return "Boom!"
+        } else {
+        return (
+            <p>{this.props.initialCount} seconds left before I go boom!</p>
+        )
+        }
+}
+    }
+
+
+export default Bomb
--- src/ImageSlider.js
+++ src/ImageSlider.js
@@ -1,1 +1,32 @@
 // your ImageSlider code here!
+import React from 'react'
+
+
+
+class ImageSlider extends React.Component {
+
+    constructor() {
+        super() 
+        this.state = {
+            currentSlideIndex: 0
+        }}
+
+        increment = () => {
+            const newCount = this.state.count + 1
+            this.setState({
+                currentSlideIndex: newCount
+            })
+        }
+
+
+
+    render () {
+        return (
+            <p>I am on slide {this.state.currentSlideIndex}</p>
+            )
+         
+      
+}
+}
+
+export default ImageSlider
