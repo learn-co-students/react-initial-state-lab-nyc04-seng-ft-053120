38490a1b3cf2704474ead18fbfe5442aece3433a
Rouan Thompson <rouan.thompson@gmail.com>
2020-08-25 07:54:38 -0400

    Done.

--- src/Bomb.js
+++ src/Bomb.js
@@ -1,1 +1,35 @@
 // your Bomb code here!
+//allow this file to use code from another file
+import React from 'react';
+
+//super() is needed for constructors()....
+// super() must be called before any use of 'this' keyword
+// super() refers to the parent's class
+
+class Bomb extends React.Component {
+
+    //constructo(): creates and initalize an object of that class
+    //custom initialization will be done before any other methods
+    //constructor args can be ommited. if one is given: slider = new ImageSlider('tiptop')
+    //..tiptop is passed to the constructor to be used as its argument
+    //this is refering to the new object to be created
+
+    constructor(props) {
+        super()
+        this.state = {
+            secondsLeft: props.initialCount
+        }
+    }
+    
+    render(){
+        return(
+        <div>
+            {this.state.secondsLeft > 0 ? `${this.state.secondsLeft} seconds left before I go boom!`: "Boom!"}
+        </div>
+        )
+    }
+}
+
+
+//allows this file to be used in another file
+export default Bomb
--- src/ImageSlider.js
+++ src/ImageSlider.js
@@ -1,1 +1,46 @@
 // your ImageSlider code here!
+import React from 'react';
+
+//super() is needed within constructors()....
+// super() must be called before any use of 'this' keyword
+// super() refers to the parent's class
+class ImageSlider extends React.Component {
+
+    //constructo(): creates and initalize an object of that class
+    //custom initialization will be done before any other methods
+    //constructor args can be ommited. if one is given: slider = new ImageSlider('tiptop')
+    //..tiptop is passed to the constructor to be used as its argument
+    //this is refering to the new object to be created
+
+    constructor() {
+        super()
+        this.state = {
+            currentSlideIndex: 0
+        }
+    }
+    render() {
+        return (
+        <div>I am on slide {this.state.currentSlideIndex}</div>
+        )
+    }
+}
+
+export default ImageSlider
+
+
+
+//in order to run npm test must have the following:
+
+// import React from 'react';
+
+// class ImageSlider extends React.Component {
+
+    
+//     render () {
+//         return (
+//             <div></div>
+//         )
+//     }
+// }
+
+// export default ImageSlider
